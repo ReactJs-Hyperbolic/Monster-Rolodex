9e8f1055cda165cd3f6195aa78bcda9b76b5387d
Karsenh <KarsenHansen@gmail.com>
2021-05-13 11:19:32 -0700

    Created grid layout for card children wrapper

--- src/App.js
+++ src/App.js
@@ -13,19 +13,18 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ monsters: users }));
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
       <div className='App'>
         <CardList name='Karsennn'>
-          <p>Karsen Test</p>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}> {monster.name} </h1>
+          ))}
         </CardList>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
       </div>
     );
   }
--- src/components/card-list/card-list-component.jsx
+++ src/components/card-list/card-list-component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import './card-list.styles.css'
 
-export const CardList = (props) => {
-  console.log(props);
-  return <div>{props.children}</div>;
+export const CardList = ({children}) => {
+  return <div className='card-list'>{children}</div>;
 };
